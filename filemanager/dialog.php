91aea427e3e4195d851d1ca5be07e5e45b2c3ff9
Alberto <info@albertoperipolli.com>
2013-08-12 00:26:16 +0200

    Version 7.1

--- filemanager/dialog.php
+++ filemanager/dialog.php
@@ -226,7 +226,6 @@ $get_params = http_build_query(array(
 				<button class="btn tip<?php if($view==2) echo " btn-inverse"; ?>" data-value="2" title="<?php echo lang_View_columns_list; ?>"><i class="icon-fire <?php if($view==2) echo "icon-white"; ?>"></i></button>
 			</div>
 			<div class="span6 types">
-			    <?php if($_GET['type']==2 || $_GET['type']==0){ ?>
 			    <span><?php echo lang_Filters; ?>:</span>
 			    
 			    <input id="select-type-1" name="radio-sort" type="radio" data-item="ff-item-type-1" checked="checked"  class="hide"  />
@@ -239,12 +238,12 @@ $get_params = http_build_query(array(
 			    <label id="ff-item-type-4" title="<?php echo lang_Videos; ?>" for="select-type-4" class="tip btn ff-label-type-4"><i class="icon-film"></i></label>
 			    <input id="select-type-5" name="radio-sort" type="radio" data-item="ff-item-type-5" class="hide"  />
 			    <label id="ff-item-type-5" title="<?php echo lang_Music; ?>" for="select-type-5" class="tip btn ff-label-type-5"><i class="icon-music"></i></label>
-			    <?php } ?>
+			    
 			    <input accesskey="f" type="text" class="filter-input" id="filter-input" name="filter" placeholder="<?php echo strtolower(lang_Text_filter); ?>..." value=""/>
-			    <?php if($_GET['type']==2 || $_GET['type']==0){ ?>
+			    
 			    <input id="select-type-all" name="radio-sort" type="radio" data-item="ff-item-type-all" class="hide" />
 			    <label id="ff-item-type-all" title="<?php echo lang_All; ?>" for="select-type-all" class="tip btn btn-inverse ff-label-type-all"><i class="icon-align-justify icon-white"></i></label>
-			    <?php } ?>
+			    
 			</div>
 		    </div>
 		</div>
@@ -408,7 +407,7 @@ $get_params = http_build_query(array(
 					if(file_exists('ico/file_extension_'.strtolower($file_ext).".png")){
 						$mini_src  ='ico/file_extension_'.strtolower($file_ext).".png";
 					}else{
-						$mini_src ='ico/'.strtolower($file_ext).".png";
+						$mini_src ='ico/'.strtoupper($file_ext).".png";
 					}
 				}
 				
