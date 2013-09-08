9de36dec4081e7d861a97b16f0d173b8168d963d
Alberto <info@albertoperipolli.com>
2013-09-08 11:58:02 +0200

    Version 8.1.1
    update en lang

--- filemanager/lang/en_EN.php
+++ filemanager/lang/en_EN.php
@@ -37,7 +37,7 @@ define('lang_Text_filter','text filter');
 define('lang_Swipe_help','Swipe the name of file/folder to show options');
 define('lang_Upload_base','Base upload');
 define('lang_Upload_java','JAVA upload (big size files)');
-define('lang_Upload_java_help',"If the Java Applet don't load 1. make sure you have Java installed otherwise <a href='http://java.com/en/download/'>[download link]</a> 2. make sure nothing is blocked from firewall");
+define('lang_Upload_java_help',"If the Java Applet doesn't load, 1. make sure you have Java installed, otherwise <a href='http://java.com/en/download/'>[download link]</a>   2. make sure nothing is blocked by your firewall");
 define('lang_Upload_base_help',"Drag & Drop files or click in the area above (modern browsers) and select the file(s). When the upload is complete, click the 'Return to files list' button.");
 define('lang_Type_dir','dir');
 define('lang_Type','Type');
