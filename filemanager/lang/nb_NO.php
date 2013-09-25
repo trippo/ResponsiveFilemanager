d076c54d819401ba412a3666de05918829a0c955
Alberto <info@albertoperipolli.com>
2013-09-25 11:42:35 +0200

    Version 9.1

--- filemanager/lang/nb_NO.php
+++ filemanager/lang/nb_NO.php
@@ -38,21 +38,21 @@ define('lang_Swipe_help','Sveip filnavnet/mappenavnet for å vise alternativer')
 define('lang_Upload_base','Vanlig opplasting');
 define('lang_Upload_java','Java-opplasting (store filer)');
 define('lang_Upload_java_help',"Hvis java-appleten ikke lastes: 1. Sjekk om Java er installert, hvis ikke <a href='http://java.com/en/download/'>last ned Java</a> 2. Sjekk brannmur-innstillingene.");
-define('lang_Upload_base_help',"Drag & Drop file/s inside above area or click in it (for modern browsers) otherwise select the file and click on button. When the upload end, click on upper return button.");
+define('lang_Upload_base_help',"Dra og slipp filen(e) i området over eller klikk (virker for moderne nettlesere). Ved bruk av gammel nettleser: Velg filen og klikk på knappen. Når opplastingen er ferdig, klikk på tilbake-knappen øverst.");
 define('lang_Type_dir','Mappe');
 define('lang_Type','Type');
 define('lang_Dimension','Dimensjoner');
 define('lang_Size','Størrelse');
 define('lang_Date','Dato');
 define('lang_Filename','Filnavn');
-define('lang_Operations','Operations');
+define('lang_Operations','Handlinger');
 define('lang_Date_type','d.m.y');
 define('lang_OK','OK');
 define('lang_Cancel','Avbryt');
-define('lang_Sorting','sıralama');
-define('lang_Show_url','show URL');
+define('lang_Sorting','Sortering');
+define('lang_Show_url','Vis URL');
 define('lang_Extract','extract here');
-define('lang_File_info','file info');
-define('lang_Edit_image','edit image');
+define('lang_File_info','Fil-info');
+define('lang_Edit_image','Rediger bilde');
 
 ?>
