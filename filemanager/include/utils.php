8e3fc4431834f676bdd46862a40df27db9ed7c3a
Alberto <info@albertoperipolli.com>
2013-10-03 08:58:46 +0200

    Version 9.1.2

--- filemanager/include/utils.php
+++ filemanager/include/utils.php
@@ -153,7 +153,7 @@ function image_check_memory_usage($img, $max_breedte, $max_hoogte){
     if(file_exists($img)){
 	$K64 = 65536;    // number of bytes in 64K
 	$memory_usage = memory_get_usage();
-	$memory_limit = intval(str_replace('M','',ini_get('memory_limit'))*1024*1024);
+	$memory_limit = abs(intval(str_replace('M','',ini_get('memory_limit'))*1024*1024));
 	$image_properties = getimagesize($img);
 	$image_width = $image_properties[0];
 	$image_height = $image_properties[1];
