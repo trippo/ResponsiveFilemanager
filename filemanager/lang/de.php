03948e968c5e0055830a45c5e769d3e65d8e4aa6
johannes <johannes.wigandt@gmail.com>
2014-03-28 18:06:24 +0100

    Translated missing strings; Improvodes old translations; rewrote umlauts in HTML tags

--- filemanager/lang/de.php
+++ filemanager/lang/de.php
@@ -1,7 +1,7 @@
 <?php
 define('lang_Select','Ausw&auml;hlen');
 define('lang_Erase','L&ouml;schen');
-define('lang_Open','Öffnen');
+define('lang_Open','&Ouml;ffnen');
 define('lang_Confirm_del','Sind Sie sicher das Sie die Datei l&ouml;schen wollen?');
 define('lang_All','Alle');
 define('lang_Files','Dateien');
@@ -12,7 +12,7 @@ define('lang_Error_extension','Dieser Dateityp ist nicht erlaubt.');
 define('lang_Upload_file','Upload');
 define('lang_Filters','Filter');
 define('lang_Videos','Videos');
-define('lang_Music','Music');
+define('lang_Music','Musik');
 define('lang_New_Folder','Neues Verzeichniss');
 define('lang_Folder_Created','Verzeichnis erfolgreich erstellt');
 define('lang_Existing_Folder','Verzeichnis schließen');
@@ -27,51 +27,51 @@ define('lang_Back','zur&uuml;ck');
 define('lang_View','Ansicht');
 define('lang_View_list','Listenansicht');
 define('lang_View_columns_list','Spaltenansicht');
-define('lang_View_boxes','Box Ansicht');
+define('lang_View_boxes','Gitteransicht');
 define('lang_Toolbar','Toolbar');
 define('lang_Actions','Aktionen');
 define('lang_Rename_existing_file','Die Datei existiert bereits');
 define('lang_Rename_existing_folder','Das Verzeichnis existiert bereits');
 define('lang_Empty_name','Der Name ist leer');
 define('lang_Text_filter','Text Filter');
-define('lang_Swipe_help','Fahren Sie mit der Maus über die Datei um Details anzeigen zu lassen.');
+define('lang_Swipe_help','Fahren Sie mit der Maus &uuml;ber die Datei um Details anzeigen zu lassen.');
 define('lang_Upload_base','Base upload');
 define('lang_Upload_java','JAVA upload (gro&szlig;e Dateien)');
-define('lang_Upload_java_help',"If the Java Applet doesn't load, 1. make sure you have Java installed, otherwise <a href='http://java.com/en/download/'>[download link]</a> 2. make sure nothing is blocked by your firewall");
+define('lang_Upload_java_help',"Wenn diese Java-Applikation nicht geladen wird, 1. stellen Sie sicher, dass Java installiert ist, ansonsten <a href='http://java.com/en/download/'>[Downloaden Sie]</a> 2. stellen Sie sicher, dass nichts von Ihrer Firewall blockiert wird.");
 define('lang_Upload_base_help',"Klicken Sie in dieses Feld oder ziehen Sie die gew&uuml;nschte Datei(en) per Drag & Drop.");
 define('lang_Type_dir','dir');
 define('lang_Type','Typ');
 define('lang_Dimension','Dimensionen');
-define('lang_Size','Grö&szlig;e');
+define('lang_Size','Gr&ouml;&szlig;e');
 define('lang_Date','Datum');
 define('lang_Filename','Dateiname');
 define('lang_Operations','Operationen');
 define('lang_Date_type','d-m-y');
 define('lang_OK','OK');
 define('lang_Cancel','Abbrechen');
-define('lang_Sorting','sortiere');
+define('lang_Sorting','Sortierung');
 define('lang_Show_url','zeige URL');
 define('lang_Extract','hier entpacken');
 define('lang_File_info','Datei Info');
-define('lang_Edit_image','edit image');
-define('lang_Duplicate','Doppelt');
-define('lang_Folders', 'Folders');
-define('lang_Copy','Copy');
-define('lang_Cut','Cut');
-define('lang_Paste','Paste');
-define('lang_CB', 'CB'); // clipboard
-define('lang_Paste_Here','Paste to this directory');
-define('lang_Paste_Confirm','Are you sure you want to paste to this directory? This will overwrite existing files/folders if encountered any.');
-define('lang_Paste_Failed','Failed to paste file(s)');
-define('lang_Clear_Clipboard','Clear clipboard');
-define('lang_Clear_Clipboard_Confirm','Are you sure you want to clear the clipboard?');
-define('lang_Files_ON_Clipboard','There are files on the clipboard.');
-define('lang_Copy_Cut_Size_Limit','The selected files/folders are too big to %s. Limit: %d MB/operation'); // %s = cut or copy
-define('lang_Copy_Cut_Count_Limit','You selected too many files/folders to %s. Limit: %d files/operation'); // %s = cut or copy
-define('lang_Copy_Cut_Not_Allowed','You are not allowed to %s files.'); // %s(1) = cut or copy, %s(2) = files or folders
-define('lang_Aviary_No_Save', 'Could not save image');
-define('lang_Zip_No_Extract', 'Could not extract. File might be corrupt.');
-define('lang_Zip_Invalid', 'This extension is not supported. Valid: zip, gz, tar.');
-define('lang_Dir_No_Write', 'The directory you selected is not writable.');
-define('lang_Function_Disabled', 'The %s function has been disabled by the server.'); // %s = cut or copy
+define('lang_Edit_image','Bild bearbeiten');
+define('lang_Duplicate','Duplizieren');
+define('lang_Folders', 'Verzeichnisse');
+define('lang_Copy','Kopieren');
+define('lang_Cut','Ausschneiden');
+define('lang_Paste','Einf&uuml;gen');
+define('lang_CB', 'Zwischenablage'); // clipboard
+define('lang_Paste_Here','In dieses Verzeichnis einf&uuml;gen');
+define('lang_Paste_Confirm','Sind Sie sicher, dass Sie in dieses Verzeichnis einf&uuml;gen m&ouml;chten? Damit werden bestehende Dateien / Ordner &uuml;berschrieben falls bereits vorhanden.');
+define('lang_Paste_Failed','Fehler beim Einf&uuml;gen von Datei(en)');
+define('lang_Clear_Clipboard','Zwischenablage l&ouml;schen');
+define('lang_Clear_Clipboard_Confirm','Sind Sie sicher, dass Sie die Zwischenablage l&ouml;schen m&ouml;chten?');
+define('lang_Files_ON_Clipboard','Es gibt Dateien in der Zwischenablage.');
+define('lang_Copy_Cut_Size_Limit','Die ausgew&auml;hlten Dateien / Ordner sind zu groß um diese %s. Limit: %d MB / Operation'); // %s = cut or copy
+define('lang_Copy_Cut_Count_Limit','Sie haben zu viele Dateien / Ordner zum %s ausgew&auml;hlt. Limit: %d Dateien / Operation'); // %s = cut or copy
+define('lang_Copy_Cut_Not_Allowed','Sie sind nicht berechtigt Dateien %s.'); // %s(1) = cut or copy, %s(2) = files or folders
+define('lang_Aviary_No_Save', 'Bild konnte nicht gespeichert werden');
+define('lang_Zip_No_Extract', 'Konnte nicht extrahiert werden. Datei k&ouml;nnte besch&auml;digt sein.');
+define('lang_Zip_Invalid', 'Diese Dateiendung wird nicht g&uuml;ltig. G&uuml;ltig: zip, gz, tar.');
+define('lang_Dir_No_Write', 'Die ausgew&auml;hlte Verzeichnis ist nicht beschreibbar.');
+define('lang_Function_Disabled', 'Die %s Funktion wurde vom Server deaktiviert.'); // %s = cut or copy
 ?>
