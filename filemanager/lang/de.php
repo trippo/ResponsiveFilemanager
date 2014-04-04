d2091ca4aed6bf56c8f9a5aa01a69636b139452f
Alberto <trippo@hotmail.it>
2014-04-04 08:40:35 +0200

    german lang

--- filemanager/lang/de.php
+++ filemanager/lang/de.php
@@ -2,76 +2,76 @@
 define('lang_Select','Ausw&auml;hlen');
 define('lang_Erase','L&ouml;schen');
 define('lang_Open','&Ouml;ffnen');
-define('lang_Confirm_del','Sind Sie sicher, dass Sie diese Datei l&ouml;schen möchten?');
+define('lang_Confirm_del','Sind Sie sicher das Sie die Datei l&ouml;schen wollen?');
 define('lang_All','Alle');
 define('lang_Files','Dateien');
 define('lang_Images','Bilder');
 define('lang_Archives','Archive');
-define('lang_Error_Upload','Die Date &uuml;berschreitet die maximal erlaubte Gr&ouml;ße.');
+define('lang_Error_Upload','Die Datei &uuml;berschreitet die maximal erlaubte Gr&ouml;ße.');
 define('lang_Error_extension','Dieser Dateityp ist nicht erlaubt.');
-define('lang_Upload_file','Upload');
+define('lang_Upload_file','Hochladen');
 define('lang_Filters','Filter');
 define('lang_Videos','Videos');
 define('lang_Music','Musik');
-define('lang_New_Folder','Neuer Ordner');
-define('lang_Folder_Created','Ordner erfolgreich erstellt');
-define('lang_Existing_Folder','Vorhandener Ordner');
-define('lang_Confirm_Folder_del','Sind Sie sicher, dass Sie diesen Ordner und alle darin enthaltene Dateien l&ouml;schen m&ouml;chten?');
+define('lang_New_Folder','Neues Verzeichniss');
+define('lang_Folder_Created','Verzeichnis erfolgreich erstellt');
+define('lang_Existing_Folder','Verzeichnis schließen');
+define('lang_Confirm_Folder_del','Sind Sie sicher das Sie dieses Verzeichnis inklusive aller darin enthaltenen Dateien l&ouml;schen m&ouml;chten?');
 define('lang_Return_Files_List','Zur&uuml;ck zur Datei&uuml;bersicht');
 define('lang_Preview','Vorschau');
 define('lang_Download','Download');
-define('lang_Insert_Folder_Name','Ordnernamen angeben:');
-define('lang_Root','root');
+define('lang_Insert_Folder_Name','Verzeichnisnamen angeben:');
+define('lang_Root','Basisverzeichnis');
 define('lang_Rename','Umbenenen');
-define('lang_Back','Zur&uuml;ck');
+define('lang_Back','zur&uuml;ck');
 define('lang_View','Ansicht');
 define('lang_View_list','Listenansicht');
 define('lang_View_columns_list','Spaltenansicht');
-define('lang_View_boxes','Gitteransicht');
-define('lang_Toolbar','Toolbar');
+define('lang_View_boxes','Box Ansicht');
+define('lang_Toolbar','Werkzeugleiste');
 define('lang_Actions','Aktionen');
 define('lang_Rename_existing_file','Die Datei existiert bereits');
-define('lang_Rename_existing_folder','Der Ordner existiert bereits');
+define('lang_Rename_existing_folder','Das Verzeichnis existiert bereits');
 define('lang_Empty_name','Der Name ist leer');
 define('lang_Text_filter','Text Filter');
-define('lang_Swipe_help','Fahren Sie mit der Maus &uuml;ber die Datei um Details anzeigen zu lassen.');
-define('lang_Upload_base','Basis upload');
-define('lang_Upload_java','JAVA upload (gro&szlig;e Dateien)');
-define('lang_Upload_java_help',"Wenn diese Java-Applikation nicht geladen wird, 1. stellen Sie sicher, dass Java installiert ist, ansonsten <a href='http://java.com/en/download/'>[downloaden Sie]</a> 2. stellen Sie sicher, dass nichts von Ihrer Firewall blockiert wird.");
+define('lang_Swipe_help','Fahren Sie mit der Maus über die Datei um Details anzeigen zu lassen.');
+define('lang_Upload_base','Basis Upload');
+define('lang_Upload_java','Java upload (gro&szlig;e Dateien)');
+define('lang_Upload_java_help',"Wenn das Java Applet nicht l&auml;dt, 1. versichern Sie sich der aktuellen Java-Installation, download hier: <a href='http://java.com/en/download/'>[download link]</a> 2. &uuml;berpr&uuml;fen Sie Ihre Firewall-Einstellungen.");
 define('lang_Upload_base_help',"Klicken Sie in dieses Feld oder ziehen Sie die gew&uuml;nschte Datei(en) per Drag & Drop.");
 define('lang_Type_dir','dir');
 define('lang_Type','Typ');
 define('lang_Dimension','Dimensionen');
-define('lang_Size','Gr&ouml;&szlig;e');
+define('lang_Size','Grö&szlig;e');
 define('lang_Date','Datum');
 define('lang_Filename','Dateiname');
 define('lang_Operations','Operationen');
-define('lang_Date_type','d.m.Y');
+define('lang_Date_type','d.m.y');
 define('lang_OK','OK');
 define('lang_Cancel','Abbrechen');
-define('lang_Sorting','Sortierung');
+define('lang_Sorting','sortiere');
 define('lang_Show_url','zeige URL');
 define('lang_Extract','hier entpacken');
 define('lang_File_info','Datei Info');
-define('lang_Edit_image','Bild bearbeiten');
-define('lang_Duplicate','Duplizieren');
+define('lang_Edit_image','Bild editieren');
+define('lang_Duplicate','Doppelt');
 define('lang_Folders', 'Ordner');
 define('lang_Copy','Kopieren');
-define('lang_Cut','Ausschneiden');
+define('lang_Cut','Aussschneiden');
 define('lang_Paste','Einf&uuml;gen');
-define('lang_CB', 'Zwischenablage'); // clipboard
-define('lang_Paste_Here','In diesen Ordner einf&uuml;gen');
-define('lang_Paste_Confirm','Sind Sie sicher, dass Sie in diesen Ordner einf&uuml;gen m&ouml;chten? Damit werden bestehende Dateien / Ordner &uuml;berschrieben falls diese bereits vorhanden sind.');
-define('lang_Paste_Failed','Fehler beim Einf&uuml;gen von Datei(en)');
-define('lang_Clear_Clipboard','Zwischenablage l&ouml;schen');
-define('lang_Clear_Clipboard_Confirm','Sind Sie sicher, dass Sie die Zwischenablage l&ouml;schen m&ouml;chten?');
-define('lang_Files_ON_Clipboard','Es gibt Dateien in der Zwischenablage.');
-define('lang_Copy_Cut_Size_Limit','Die ausgew&auml;hlten Dateien / Ordner sind zu groß um diese %s. Limit: %d MB / Operation'); // %s = cut or copy
-define('lang_Copy_Cut_Count_Limit','Sie haben zu viele Dateien / Ordner zum %s ausgew&auml;hlt. Limit: %d Dateien / Operation'); // %s = cut or copy
-define('lang_Copy_Cut_Not_Allowed','Sie sind nicht berechtigt Dateien %s.'); // %s(1) = cut or copy, %s(2) = files or folders
-define('lang_Aviary_No_Save', 'Bild konnte nicht gespeichert werden');
-define('lang_Zip_No_Extract', 'Konnte nicht extrahiert werden. Datei k&ouml;nnte besch&auml;digt sein.');
-define('lang_Zip_Invalid', 'Diese Dateiendung ist nicht g&uuml;ltig. G&uuml;ltig: zip, gz, tar.');
-define('lang_Dir_No_Write', 'Der ausgew&auml;hlte Ordner ist nicht beschreibbar.');
-define('lang_Function_Disabled', 'Die %s Funktion wurde vom Server deaktiviert.'); // %s = cut or copy
+define('lang_CB', 'CB'); // clipboard
+define('lang_Paste_Here','Aus der Zwischenablage einf&uuml;gen');
+define('lang_Paste_Confirm','Sind Sie sicher, dass Sie es in diesen Ordner einf&uuml;gen möchten? Existierende Dateien/Ordner files/folders if encountered any.');
+define('lang_Paste_Failed','Einf&uuml;gen fehgeschlagen!');
+define('lang_Clear_Clipboard','Clear clipboard');
+define('lang_Clear_Clipboard_Confirm','Zwischenablage wirkich leeren?');
+define('lang_Files_ON_Clipboard','Sie haben bereits Dateien in der Zwischenablage!');
+define('lang_Copy_Cut_Size_Limit','Die Dateien sind zu gross, um sie %s. Limit: %d MB pro Aktion'); // %s = cut or copy
+define('lang_Copy_Cut_Count_Limit','Sie haben zu viele Dateien gew&auml;hlt. Limit: %d Dateien pro Aktion'); // %s = cut or copy
+define('lang_Copy_Cut_Not_Allowed','Die Dateien k&ouml;nnen so nicht veabeitet werden.'); // %s(1) = cut or copy, %s(2) = files or folders
+define('lang_Aviary_No_Save', 'Das Bild konnte nicht gespeichert werden.');
+define('lang_Zip_No_Extract', 'Zip Datei konnte nicht entpackt werden. Es ist m&ouml;glicherweise besch&auml;digt.');
+define('lang_Zip_Invalid', 'Dieses Dateiformat wird nicht unterst&uuml;zt. Zugelassene Formate: zip, gz und tar.');
+define('lang_Dir_No_Write', 'Dieses Dateiverzeichis ist schreibgesch&uuml;tzt.');
+define('lang_Function_Disabled', 'Ausschneiden und Kopieren ist serverseitig deaktiviert.'); // %s = cut or copy
 ?>
