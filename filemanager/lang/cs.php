5a4f99c97401e1bd5d70e125ffd8e92b68f25d1a
Alberto <info@tecrail.com>
2014-06-20 13:22:28 +0200

    Czech translation
    by Piki Jan

--- filemanager/lang/cs.php
+++ filemanager/lang/cs.php
@@ -8,7 +8,7 @@ define('lang_Files','Soubory');
 define('lang_Images','Obrázky');
 define('lang_Archives','Archivy');
 define('lang_Error_Upload','Nahrávaný soubor je příliš velký.');
-define('lang_Error_extension','Nahrání souborů s touto příponou není povoleno.');
+define('lang_Error_extension','Nahrávání souborů s touto příponou není povoleno.');
 define('lang_Upload_file','Nahrát soubor');
 define('lang_Filters','Filtr');
 define('lang_Videos','Videa');
@@ -23,10 +23,10 @@ define('lang_Download','Stáhnout');
 define('lang_Insert_Folder_Name','Vložte název složky:');
 define('lang_Root','root');
 define('lang_Rename','Přejmenovat');
-define('lang_Back','zpátky');
+define('lang_Back','zpět');
 define('lang_View','Zobrazení');
 define('lang_View_list','Seznam souborů');
-define('lang_View_columns_list','Dvousloucpvý seznam souborů');
+define('lang_View_columns_list','Dvousloupcový seznam souborů');
 define('lang_View_boxes','Dlaždicové zobrazení');
 define('lang_Toolbar','Panel nástrojů');
 define('lang_Actions','Akce');
@@ -55,43 +55,43 @@ define('lang_Extract','Rozbalit zde');
 define('lang_File_info','Informace o souboru');
 define('lang_Edit_image','Upravit obrázek');
 define('lang_Duplicate','Zduplikovat');
-define('lang_Folders', 'Folders');
-define('lang_Copy','Copy');
-define('lang_Cut','Cut');
-define('lang_Paste','Paste');
-define('lang_CB', 'CB'); // clipboard
-define('lang_Paste_Here','Paste to this directory');
-define('lang_Paste_Confirm','Are you sure you want to paste to this directory? This will overwrite existing files/folders if encountered any.');
-define('lang_Paste_Failed','Failed to paste file(s)');
-define('lang_Clear_Clipboard','Clear clipboard');
-define('lang_Clear_Clipboard_Confirm','Are you sure you want to clear the clipboard?');
-define('lang_Files_ON_Clipboard','There are files on the clipboard.');
-define('lang_Copy_Cut_Size_Limit','The selected files/folders are too big to %s. Limit: %d MB/operation'); // %s = cut or copy
-define('lang_Copy_Cut_Count_Limit','You selected too many files/folders to %s. Limit: %d files/operation'); // %s = cut or copy
-define('lang_Copy_Cut_Not_Allowed','You are not allowed to %s files.'); // %s(1) = cut or copy, %s(2) = files or folders
-define('lang_Aviary_No_Save', 'Could not save image');
-define('lang_Zip_No_Extract', 'Could not extract. File might be corrupt.');
-define('lang_Zip_Invalid', 'This extension is not supported. Valid: zip, gz, tar.');
-define('lang_Dir_No_Write', 'The directory you selected is not writable.');
-define('lang_Function_Disabled', 'The %s function has been disabled by the server.'); // %s = cut or copy
-define('lang_File_Permission', 'File permission');
-define('lang_File_Permission_Not_Allowed', 'Changing %s permissions are not allowed.'); // %s = files or folders
-define('lang_File_Permission_Recursive', 'Apply recursively?');
-define('lang_File_Permission_Wrong_Mode', "The supplied permission mode is incorrect.");
-define('lang_User', 'User');
-define('lang_Group', 'Group');
-define('lang_Yes', 'Yes');
-define('lang_No', 'No');
-define('lang_Lang_Not_Found', 'Could not find language.');
-define('lang_Lang_Change', 'Change the language');
-define('lang_File_Not_Found', 'Could not find the file.');
-define('lang_File_Open_Edit_Not_Allowed', 'You are not allowed to %s this file.'); // %s = open or edit
-define('lang_Edit', 'Edit');
-define('lang_Edit_File', "Edit file's content");
-define('lang_File_Save_OK', "File successfully saved.");
-define('lang_File_Save_Error', "There was an error while saving the file.");
-define('lang_New_File','New File');
-define('lang_No_Extension','You have to add a file extension.'); 
-define('lang_Valid_Extensions','Valid extensions: %s'); // %s = txt,log etc.
+define('lang_Folders', 'Složky');
+define('lang_Copy','Kopírovat');
+define('lang_Cut','Vyjmout');
+define('lang_Paste','Vložit');
+define('lang_CB', 'Schránka'); // clipboard
+define('lang_Paste_Here','Vložit do této složky');
+define('lang_Paste_Confirm','Skutečně vložit obsah schránky do této složky? Pokud se objeví existující soubory či složky, budou přepsány.');
+define('lang_Paste_Failed','Vložení selhalo');
+define('lang_Clear_Clipboard','Vymazat schránku');
+define('lang_Clear_Clipboard_Confirm','Skutečně chcete vymazat obsah schránky?');
+define('lang_Files_ON_Clipboard','Ve schránce jsou soubory.');
+define('lang_Copy_Cut_Size_Limit','Zvolené soubory/složky jsou příliš velké pro operaci %s. Limit: %d MB/operace'); // %s = cut or copy
+define('lang_Copy_Cut_Count_Limit','Zvolili jste příliš mnoho souborů/složek pro operaci %s. Limit: %d souborů/operace'); // %s = cut or copy
+define('lang_Copy_Cut_Not_Allowed','Nemáte oprávnění %s soubory.'); // %s(1) = cut or copy, %s(2) = files or folders
+define('lang_Aviary_No_Save', 'Obrázek nelze uložit');
+define('lang_Zip_No_Extract', 'Nelze rozbalit. Soubor může být poškozen.');
+define('lang_Zip_Invalid', 'Přípona není podporována. Povolené: zip, gz, tar.');
+define('lang_Dir_No_Write', 'Adresář, který jste vybrali, není zapisovatelný.');
+define('lang_Function_Disabled', 'Funkce %s byla zamítnuta serverem.'); // %s = cut or copy
+define('lang_File_Permission', 'Práva souboru');
+define('lang_File_Permission_Not_Allowed', 'Změna oprávnění pro %s není povolena.'); // %s = files or folders
+define('lang_File_Permission_Recursive', 'Použít rekurzivně?');
+define('lang_File_Permission_Wrong_Mode', "Požadujete nesprávný mód oprávnění.");
+define('lang_User', 'Uživatel');
+define('lang_Group', 'Skupina');
+define('lang_Yes', 'Ano');
+define('lang_No', 'Ne');
+define('lang_Lang_Not_Found', 'Jazyk nelze najít.');
+define('lang_Lang_Change', 'Změnit jazyk');
+define('lang_File_Not_Found', 'Soubor nelze najít.');
+define('lang_File_Open_Edit_Not_Allowed', 'Nemáte oprávnění %s tento soubor.'); // %s = open or edit
+define('lang_Edit', 'Upravit');
+define('lang_Edit_File', "Upravit obsah soubor");
+define('lang_File_Save_OK', "Soubor byl úspěšně uložen.");
+define('lang_File_Save_Error', "Došlo k chybě při ukládání souboru.");
+define('lang_New_File','Nový soubor');
+define('lang_No_Extension','Musíte přidat příponu souboru.');
+define('lang_Valid_Extensions','Platné přípony: %s'); // %s = txt,log etc.
 
 ?>
