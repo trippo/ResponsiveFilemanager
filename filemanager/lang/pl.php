3d6e5c016d4d9c89587e1a5f9a1449e0bae11153
Alberto <info@tecrail.com>
2014-06-25 13:52:24 +0200

    Polish translation

--- filemanager/lang/pl.php
+++ filemanager/lang/pl.php
@@ -74,24 +74,24 @@ define('lang_Zip_No_Extract', 'Archiwum ZIP nie może zostać rozpakowane tam. P
 define('lang_Zip_Invalid', 'Te rozszerzenia plików nie posiadają tutaj naszego wspierane. Dopuszczamy tylko format: zip, gz, tar.');
 define('lang_Dir_No_Write', 'Folder który wybrałeś, nie posiada uprawnień chmod umożliwiających poprawny zapis.');
 define('lang_Function_Disabled', 'Operacja %s została zablokowana przez oprogramowanie Twojego serwera.'); // %s = cut or copy
-define('lang_File_Permission', 'File permission');
-define('lang_File_Permission_Not_Allowed', 'Changing %s permissions are not allowed.'); // %s = files or folders
-define('lang_File_Permission_Recursive', 'Apply recursively?');
-define('lang_File_Permission_Wrong_Mode', "The supplied permission mode is incorrect.");
-define('lang_User', 'User');
-define('lang_Group', 'Group');
-define('lang_Yes', 'Yes');
-define('lang_No', 'No');
-define('lang_Lang_Not_Found', 'Could not find language.');
-define('lang_Lang_Change', 'Change the language');
-define('lang_File_Not_Found', 'Could not find the file.');
-define('lang_File_Open_Edit_Not_Allowed', 'You are not allowed to %s this file.'); // %s = open or edit
-define('lang_Edit', 'Edit');
-define('lang_Edit_File', "Edit file's content");
-define('lang_File_Save_OK', "File successfully saved.");
-define('lang_File_Save_Error', "There was an error while saving the file.");
-define('lang_New_File','New File');
-define('lang_No_Extension','You have to add a file extension.'); 
-define('lang_Valid_Extensions','Valid extensions: %s'); // %s = txt,log etc.
+define('lang_File_Permission', 'Uprawnienia pliku');
+define('lang_File_Permission_Not_Allowed', 'Zmiana uprawnień dla %s jest niedozwolona.'); // %s = files or folders
+define('lang_File_Permission_Recursive', 'Zastosować rekursywnie?');
+define('lang_File_Permission_Wrong_Mode', "Zastosowane uprawnienia są niepoprawne.");
+define('lang_User', 'Użytkownik');
+define('lang_Group', 'Grupa');
+define('lang_Yes', 'Tak');
+define('lang_No', 'Nie');
+define('lang_Lang_Not_Found', 'Nie znaleziono języka.');
+define('lang_Lang_Change', 'Zmień język');
+define('lang_File_Not_Found', 'Nie można znaleźć pliku.');
+define('lang_File_Open_Edit_Not_Allowed', 'Nie masz uprawnien do pliku %s.'); // %s = open or edit
+define('lang_Edit', 'Edytuj');
+define('lang_Edit_File', "Edytuj zawartość pliku");
+define('lang_File_Save_OK', "Plik został zapisany.");
+define('lang_File_Save_Error', "Wystąpił błąd podczas zapisywania pliku.");
+define('lang_New_File','Utwórz plik');
+define('lang_No_Extension','Musisz dodać rozszerzenie do pliku.'); 
+define('lang_Valid_Extensions','Poprawne rozszerzenia: %s'); // %s = txt,log etc.
 
 ?>
