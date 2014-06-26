d578c3a68f2ec7f3808d1c975788c13de64b0991
Alberto <info@tecrail.com>
2014-06-26 16:17:45 +0200

    fix

--- filemanager/js/include.js
+++ filemanager/js/include.js
@@ -991,69 +991,70 @@ function swipe_reaction(event, direction, distance, duration, fingerCount) {
 }
 
 function apply(file,external){
-    if ($('#popup').val()==1) var window_parent=window.opener; else var window_parent=window.parent;
-    var path = $('#cur_dir').val();    
-    //path = path.replace('\\', '/');
-    var base_url = $('#base_url').val();
-    var alt_name=file.substr(0, file.lastIndexOf('.'));
-    var ext=file.split('.').pop();
-    ext=ext.toLowerCase();
-    var fill='';
-    var ext_audio=new Array('ogg','mp3','wav');
-    var ext_video=new Array('mp4','ogg','webm');
-    if (external!=""){
+  if ($('#popup').val()==1) var window_parent=window.opener; else var window_parent=window.parent;
+  var path = $('#cur_dir').val();    
+  //path = path.replace('\\', '/');
+  var base_url = $('#base_url').val();
+  var alt_name=file.substr(0, file.lastIndexOf('.'));
+  var ext=file.split('.').pop();
+  ext=ext.toLowerCase();
+  var fill='';
+  var ext_audio=new Array('ogg','mp3','wav');
+  var ext_video=new Array('mp4','ogg','webm');
+  if (external!=""){
 		if ($('#crossdomain').val()==1){
-			window.parent.postMessage({
-					sender: 'filemanager',
-					url: base_url+path+file,
-					field_id : external
-				},
-				'*'
+			window_parent.postMessage({
+				sender: 'responsivefilemanager',
+				url: base_url+path+file,
+				field_id : external
+			},
+			'*'
 			);
-      } else {
+    } else {
 			var target = $('#'+external, window_parent.document);
 			target.val(base_url+path+file).trigger('change');
 			close_window();
 		}
+  }else{
+	  if ($.inArray(ext, ext_img) > -1){
+	    fill='<img src="'+base_url+path+file+'" alt="'+alt_name+'" />';
+	  }else {
+			if ($.inArray(ext, ext_video) > -1){
+			  fill='<video controls source src="'+base_url+path+file+'" type="video/'+ext+'">'+alt_name+'</video>';
+			}else {
+			  if ($.inArray(ext, ext_audio) > -1 ){
+					if (ext=='mp3') { ext='mpeg'; }
+					fill='<audio controls src="'+base_url+path+file+'" type="audio/'+ext+'">'+alt_name+'</audio>';
+			  }else {
+					fill='<a href="'+base_url+path+file+'" title="'+alt_name+'">'+alt_name+'</a>';
+			  }
+			}
+		
 	  }
-    if ($.inArray(ext, ext_img) > -1){
-        fill='<img src="'+base_url+path+file+'" alt="'+alt_name+'" />';
-    }else {
-	if ($.inArray(ext, ext_video) > -1){
-	    fill='<video controls source src="'+base_url+path+file+'" type="video/'+ext+'">'+alt_name+'</video>';
-	}else {
-	    if ($.inArray(ext, ext_audio) > -1 ){
-		if (ext=='mp3') { ext='mpeg'; }
-		fill='<audio controls src="'+base_url+path+file+'" type="audio/'+ext+'">'+alt_name+'</audio>';
-	    }else {
-		fill='<a href="'+base_url+path+file+'" title="'+alt_name+'">'+alt_name+'</a>';
-	    }
-	}
-	
-    }
 
-	if ($('#crossdomain').val()==1){
-		window.parent.postMessage({
-				sender: 'filemanager',
-				url: base_url+path+file,
-				id : null,
-				html: fill
-			},
-			'*'
-		);
+		if ($('#crossdomain').val()==1){
+			window.parent.postMessage({
+					sender: 'responsivefilemanager',
+					url: base_url+path+file,
+					id : null,
+					html: fill
+				},
+				'*'
+			);
 
-	} else {
-		// tinymce 3.X
-		if ( parent.tinymce.majorVersion < 4 )
-		{
-			parent.tinymce.activeEditor.execCommand('mceInsertContent', false, fill); 
-			parent.tinymce.activeEditor.windowManager.close( parent.tinymce.activeEditor.windowManager.params.mce_window_id );
-		}
-		// tinymce 4.X
-		else 
-		{
-			parent.tinymce.activeEditor.insertContent(fill);
-			parent.tinymce.activeEditor.windowManager.close();
+		} else {
+			// tinymce 3.X
+			if ( parent.tinymce.majorVersion < 4 )
+			{
+				parent.tinymce.activeEditor.execCommand('mceInsertContent', false, fill); 
+				parent.tinymce.activeEditor.windowManager.close( parent.tinymce.activeEditor.windowManager.params.mce_window_id );
+			}
+			// tinymce 4.X
+			else 
+			{
+				parent.tinymce.activeEditor.insertContent(fill);
+				parent.tinymce.activeEditor.windowManager.close();
+			}
 		}
 	}
 }
@@ -1065,35 +1066,35 @@ function apply_link(file,external){
     var path = $('#cur_dir').val();
     path = path.replace('\\', '/');
     var base_url = $('#base_url').val();
-    if (external!=""){
-		if ($('#crossdomain').val()==1){
-			window.parent.postMessage({
-					sender: 'filemanager',
-					url: base_url+path+file,
-					id : external
-				},
-				'*'
-			);
-      } else {
-			var target = $('#'+external, window_parent.document);
-			target.val(base_url+path+file).trigger('change');
-			close_window();
-		}
+    if (external!=""){    	
+			if ($('#crossdomain').val()==1){
+				window_parent.postMessage({
+						sender: 'responsivefilemanager',
+						url: base_url+path+file,
+						field_id : external
+					},
+					'*'
+				);
+	    } else {
+				var target = $('#'+external, window_parent.document);
+				target.val(base_url+path+file).trigger('change');
+				close_window();
+			}
+    }else{
+			apply_any(base_url+path, file);
     }
-    else
-	apply_any(base_url+path, file);
 }
 
 function apply_img(file,external){
-    if ($('#popup').val()==1) var window_parent=window.opener; else var window_parent=window.parent;
-    var path = $('#cur_dir').val();
-    path = path.replace('\\', '/');
-    var base_url = $('#base_url').val();
-   
-    if (external!=""){
+  if ($('#popup').val()==1) var window_parent=window.opener; else var window_parent=window.parent;
+  var path = $('#cur_dir').val();
+  path = path.replace('\\', '/');
+  var base_url = $('#base_url').val();
+ 
+  if (external!=""){
 		if ($('#crossdomain').val()==1){
-			window.parent.postMessage({
-					sender: 'filemanager',
+			window_parent.postMessage({
+					sender: 'responsivefilemanager',
 					url: base_url+path+file,
 					field_id : external
 				},
@@ -1104,33 +1105,33 @@ function apply_img(file,external){
 			target.val(base_url+path+file).trigger('change');
 			close_window();
 		}
-    }
-    else
-        apply_any(base_url+path, file);
+  }else{
+    apply_any(base_url+path, file);
+  }
 }
 
 function apply_video(file,external){
-    if ($('#popup').val()==1) var window_parent=window.opener; else var window_parent=window.parent;
-    var path = $('#cur_dir').val();
-    path = path.replace('\\', '/');
-    var base_url = $('#base_url').val();
-    if (external!=""){
+  if ($('#popup').val()==1) var window_parent=window.opener; else var window_parent=window.parent;
+  var path = $('#cur_dir').val();
+  path = path.replace('\\', '/');
+  var base_url = $('#base_url').val();
+  if (external!=""){
 		if ($('#crossdomain').val()==1){
-			window.parent.postMessage({
-					sender: 'filemanager',
+			window_parent.postMessage({
+					sender: 'responsivefilemanager',
 					url: base_url+path+file,
-					id : external
+					field_id : external
 				},
 				'*'
 			);
-      } else {
+    } else {
 			var target = $('#'+external, window_parent.document);
 			target.val(base_url+path+file).trigger('change');
 			close_window();
 		}
-    }
-    else
-	apply_any(path, file);
+  }else{
+		apply_any(path, file);
+	}
 }
 
 function apply_none(file,external){	
@@ -1138,31 +1139,31 @@ function apply_none(file,external){
 	
 	if (_this.html()!="" && _this.html()!==undefined) {
 	    
-	    $('#full-img').attr('src',decodeURIComponent(_this.attr('data-url')));
-	    if (_this.hasClass('disabled')==false){
-		show_animation();
-		$('#previewLightbox').lightbox();
-	    }
+	  $('#full-img').attr('src',decodeURIComponent(_this.attr('data-url')));
+	  if (_this.hasClass('disabled')==false){
+			show_animation();
+			$('#previewLightbox').lightbox();
+	  }
 	}else {
-	    var _this=$('li[data-name="'+file+'"]').find('.modalAV');
+	  var _this=$('li[data-name="'+file+'"]').find('.modalAV');
 
-	    $('#previewAV').removeData("modal");
-	    $('#previewAV').modal({
-		backdrop: 'static',
-		keyboard: false
-	    });
-	    if (_this.hasClass('audio')) {
-		$(".body-preview").css('height','80px');
-	    }else {
-		$(".body-preview").css('height','345px');
-	    }
+	  $('#previewAV').removeData("modal");
+	  $('#previewAV').modal({
+			backdrop: 'static',
+			keyboard: false
+	  });
+	  if (_this.hasClass('audio')) {
+			$(".body-preview").css('height','80px');
+	  }else {
+			$(".body-preview").css('height','345px');
+	  }
 	    
-	    $.ajax({
-		url: decodeURIComponent(_this.attr('data-url')),
-		success: function(data) {
-		    $(".body-preview").html(data);
-		}
-	    });
+	  $.ajax({
+			url: decodeURIComponent(_this.attr('data-url')),
+			success: function(data) {
+		  	$(".body-preview").html(data);
+			}
+	  });
 	}
 	return;
 }
@@ -1172,9 +1173,9 @@ function apply_any(path, file) {
 
 	if ($('#crossdomain').val()==1){
 		window.parent.postMessage({
-				sender: 'filemanager',
+				sender: 'responsivefilemanager',
 				url: path+file,
-				id : null
+				field_id : null
 			},
 			'*'
 		);
@@ -1198,14 +1199,15 @@ function apply_any(path, file) {
 }
 
 function close_window() {
-   if ($('#popup').val()==1) window.close();
-   else {
-	if ( typeof parent.jQuery !== "undefined" && parent.jQuery) {
-	    parent.jQuery.fancybox.close();   
-	}else {
-	    parent.$.fancybox.close();
+	if ($('#popup').val()==1){
+		window.close();
+	}else{
+		if(typeof parent.jQuery !== "undefined" && parent.jQuery) {
+		  parent.jQuery.fancybox.close();   
+		}else{
+		  parent.$.fancybox.close();
+		}
 	}
-   }
 }
 
 function apply_file_duplicate(container,name){
