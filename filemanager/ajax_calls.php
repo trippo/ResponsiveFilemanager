b0e8b4c5b6d9c538d63f6033e51ef942e0ea58e7
Alberto <info@tecrail.com>
2014-10-30 08:39:06 +0100

    Version 9.8

--- filemanager/ajax_calls.php
+++ filemanager/ajax_calls.php
@@ -395,6 +395,7 @@ if(isset($_GET['action']))
 			break;
 		case 'get_file': // preview or edit
 			$sub_action = $_GET['sub_action'];
+			$preview_mode = $_GET["preview_mode"];
 
 			if ($sub_action != 'preview' && $sub_action != 'edit'){
 				die("wrong action");
@@ -407,32 +408,58 @@ if(isset($_GET['action']))
 				die(lang_File_Not_Found);
 			}
 
-			$is_allowed = ($sub_action == 'preview' ? $preview_text_files : $edit_text_files);
-			$allowed_file_exts = ($sub_action == 'preview' ? $previewable_text_file_exts : $editable_text_file_exts);
+			if ($preview_mode == 'text') {
+				$is_allowed = ($sub_action == 'preview' ? $preview_text_files : $edit_text_files);
+				$allowed_file_exts = ($sub_action == 'preview' ? $previewable_text_file_exts : $editable_text_file_exts);
+			}
+			elseif($preview_mode == 'viewerjs') {
+				$is_allowed = $viewerjs_enabled;
+				$allowed_file_exts = $viewerjs_file_exts;
+			}
+			elseif($preview_mode == 'google') {
+				$is_allowed = $googledoc_enabled;
+				$allowed_file_exts = $googledoc_file_exts;
+			}
 
 			if (!isset($allowed_file_exts) || !is_array($allowed_file_exts)){
 				$allowed_file_exts = array();
 			}
 
-			if (!in_array($info['extension'], $allowed_file_exts) 
-				|| !isset($is_allowed) 
+			if (!in_array($info['extension'], $allowed_file_exts)
+				|| !isset($is_allowed)
 				|| $is_allowed === FALSE
-				|| !is_readable($selected_file)) 
+				|| !is_readable($selected_file))
 			{
 				die(sprintf(lang_File_Open_Edit_Not_Allowed, ($sub_action == 'preview' ? strtolower(lang_Open) : strtolower(lang_Edit))));
 			}
 
-			// get and sanities
-			$data = file_get_contents($selected_file);
-			$data = htmlspecialchars($data);
-			$data = stripslashes($data);
-
 			if ($sub_action == 'preview') {
-				// echo '<h1>'.$info['basename'].'</h1><pre>'.$data.'</pre>';
-				echo '<h2>'.$info['basename'].'</h2><textarea disabled style="width:100%;height:150px;">'.$data.'</textarea>';
+				if ($preview_mode == 'text') {
+					// get and sanities
+					$data = stripslashes(htmlspecialchars(file_get_contents($selected_file)));
+					if(!in_array($info['extension'],$previewable_text_file_exts_no_prettify)){
+						echo '<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?lang='.$info['extension'].'&skin=sunburst"></script>';
+						echo '<div class="text-center"><strong>'.$info['basename'].'</strong></div><pre class="prettyprint">'.$data.'</pre>';
+					}
+					else{
+						echo '<div class="text-center"><strong>'.$info['basename'].'</strong></div><pre class="no-prettify">'.$data.'</pre>';
+					}
+
+				}
+				elseif ($preview_mode == 'viewerjs') {
+					echo '<iframe id="viewer" src="ViewerJS/#../'.$_GET["file"].'" allowfullscreen="" webkitallowfullscreen="" class="viewer-iframe"></iframe>';
+
+				}
+				elseif ($preview_mode == 'google') {
+					$url_file=$base_url.$upload_dir.str_replace($current_path,'',$_GET["file"]);
+					$googledoc_url = urlencode($url_file);
+					$googledoc_html = "<iframe src=\"http://docs.google.com/viewer?url=".$googledoc_url."&embedded=true\" class=\"google-iframe\"></iframe>";
+					echo '<div class="text-center"><strong>'.$info['basename'].'</strong></div>'.$googledoc_html.'';
+				}
 			}
 			else {
-				echo '<textarea id="textfile_edit_area" style="width:100%;height:150px;">'.$data.'</textarea>';
+				$data = stripslashes(htmlspecialchars(file_get_contents($selected_file)));
+				echo '<textarea id="textfile_edit_area" style="width:100%;height:300px;">'.$data.'</textarea>';
 			}
 
 			break;
