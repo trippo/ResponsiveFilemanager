f0c275f79f9c8a988c9b49075cf895e8a55b09e7
Matsushita <vault.vans@gmail.com>
2015-01-09 19:12:53 +0900

    fixed langfile japanese.
    fixed lang and add some new translation.

--- filemanager/lang/ja.php
+++ filemanager/lang/ja.php
@@ -1,78 +1,97 @@
 <?php
 define('lang_Select','選択');
-define('lang_Erase','消す');
-define('lang_Open','オープン');
+define('lang_Erase','消去');
+define('lang_Open','開く');
 define('lang_Confirm_del','このファイルを削除しますか?');
 define('lang_All','全て');
 define('lang_Files','ファイル');
 define('lang_Images','イメージ');
 define('lang_Archives','アーカイブ');
-define('lang_Error_Upload','アップロードファイルが最大サイズ以内を選択お願いします。');
-define('lang_Error_extension','拡張子は許可されていません。');
+define('lang_Error_Upload','アップロード可能な最大サイズを超えています。');
+define('lang_Error_extension','拡張子が許可されていません。');
 define('lang_Upload_file','アップロード');
-define('lang_Filters','フィルター');
+define('lang_Filters','フィルタ');
 define('lang_Videos','ビデオ');
 define('lang_Music','音楽');
-define('lang_New_Folder','新フォルダ');
-define('lang_Folder_Created','フォルダ作成しました');
-define('lang_Existing_Folder','存在フォルダ');
-define('lang_Confirm_Folder_del','フォルダとフォルダの中のものを削除しますか?');
-define('lang_Return_Files_List','ファイル一覧に戻る');
+define('lang_New_Folder','新規フォルダ');
+define('lang_Folder_Created','フォルダを作成しました');
+define('lang_Existing_Folder','存在するフォルダ');
+define('lang_Confirm_Folder_del','フォルダとフォルダの中身を削除しますか?');
+define('lang_Return_Files_List','ファイルの一覧に戻る');
 define('lang_Preview','プレビュー');
 define('lang_Download','ダウンロード');
-define('lang_Insert_Folder_Name','フォルダ名追加');
+define('lang_Insert_Folder_Name','フォルダ名の追加');
 define('lang_Root','ルート');
-define('lang_Rename','リネーム');
-define('lang_Back','バック');
+define('lang_Rename','名称変更');
+define('lang_Back','戻る');
 define('lang_View','ビュー');
-define('lang_View_list','一覧ビュー');
-define('lang_View_columns_list','一覧ビューコラム');
-define('lang_View_boxes','ボックスビュー');
+define('lang_View_list','一覧表示');
+define('lang_View_columns_list','カラム表示');
+define('lang_View_boxes','ボックス表示');
 define('lang_Toolbar','ツールバー');
 define('lang_Actions','アクション');
-define('lang_Rename_existing_file','ファイルが既に存在しました。');
-define('lang_Rename_existing_folder','フォルダが既に存在しました。');
+define('lang_Rename_existing_file','このファイルはすでに存在しています。');
+define('lang_Rename_existing_folder','このフォルダはすでに存在しています。');
 define('lang_Empty_name','名前が空です');
 define('lang_Text_filter','テキストフィルタ');
-define('lang_Swipe_help','ファイル・フォルダ名を大振りして、オプションを表示する');
-define('lang_Upload_base','ベース　アップロード');
-define('lang_Upload_java','JAVA アップロード (大サイズファイル)');
-define('lang_Upload_java_help',"Java　Applet　がロード出来ない場合、１．<a href='http://java.com/en/download/'>[download link]</a> インストールしてください。　2．何にもファイアウォールでブロックされていないことを確認してください。");
-define('lang_Upload_base_help',"ファイルをドラッグ＆ドロップまたは上の領域（新のブラウザ）にをクリックして、ファイル(s)を選択しする。アップロード完了時「ファイルの一覧に戻る」ボタンをクリックしてください。");
-define('lang_Type_dir','dir');
-define('lang_Type','タイプ');
-define('lang_Dimension','ディメンション');
+define('lang_Swipe_help','ファイル・フォルダ名をスワイプしてオプションを表示する');
+define('lang_Upload_base','標準アップロード');
+define('lang_Upload_java','Java アップロード (大きいファイルサイズ)');
+define('lang_Upload_java_help',"Java　Applet がロード出来ない場合１．<a href='http://java.com/en/download/'>[download link]</a> インストールしてください。　2．ファイアウォールでブロックされていないことを確認してください。");
+define('lang_Upload_base_help',"ファイルをドラッグ＆ドロップ（モダンブラウザのみ）または上の領域をクリックしてファイルを選択してください。アップロードが完了したら「ファイルの一覧に戻る」ボタンをクリックしてください。");
+define('lang_Type_dir','ディレクトリ');
+define('lang_Type','種類');
+define('lang_Dimension','画像サイズ');
 define('lang_Size','サイズ');
 define('lang_Date','日付');
 define('lang_Filename','ファイル名');
-define('lang_Operations','オペレーションズ');
-define('lang_Date_type','y-m-d');
+define('lang_Operations','操作');
+define('lang_Date_type','Y/m/d');
 define('lang_OK','OK');
 define('lang_Cancel','キャンセル');
 define('lang_Sorting','ソート');
 define('lang_Show_url','URL表示');
-define('lang_Extract','ここに抽出');
-define('lang_File_info','ファイル info');
+define('lang_Extract','ここに解凍');
+define('lang_File_info','ファイル情報');
 define('lang_Edit_image','画像編集');
-define('lang_Duplicate','重複');
+define('lang_Duplicate','複製');
 define('lang_Folders', 'フォルダ');
 define('lang_Copy','コピー');
 define('lang_Cut','カット');
-define('lang_Paste','貼る');
-define('lang_CB', 'CB'); // clipboard
-define('lang_Paste_Here','このディレクトリに貼る');
-define('lang_Paste_Confirm','このディレクトリに貼しますか？出会場合既存のファイル/フォルダを上書きします。');
-define('lang_Paste_Failed','貼るのは失敗しました。');
-define('lang_Clear_Clipboard','リップボード　クリアク');
-define('lang_Clear_Clipboard_Confirm','リップボードをクリアクしますか?');
-define('lang_Files_ON_Clipboard','リップボードでファイルがあります。');
-define('lang_Copy_Cut_Size_Limit','選択したファイルが/フォルダは％sには大きすぎる。 リミット: %d MB/オペレーション'); // %s = cut or copy
-define('lang_Copy_Cut_Count_Limit','選択したファイルが/フォルダは％sには大きすぎる。 リミット: %d ファイル/オペレーション'); // %s = cut or copy
-define('lang_Copy_Cut_Not_Allowed','ファイルが %s 許可がない。'); // %s(1) = cut or copy, %s(2) = files or folders
-define('lang_Aviary_No_Save', 'イメージを保存出来ない');
-define('lang_Zip_No_Extract', 'エキス出来ない。ファイルが破損している可能性があり');
-define('lang_Zip_Invalid', '拡張子はサポートされません。有効：　zip, gz, tar.');
-define('lang_Dir_No_Write', '選択したディレクトリは書くことのできない。');
-define('lang_Function_Disabled', '%s 機能はサーバによって無効にされている'); // %s = cut or copy
+define('lang_Paste','ペースト');
+define('lang_CB', 'クリップボード'); // clipboard
+define('lang_Paste_Here','このディレクトリにペーストする');
+define('lang_Paste_Confirm','このディレクトリにペーストしますか？既存のファイル/フォルダは上書きされます。');
+define('lang_Paste_Failed','ペーストできませんでした。');
+define('lang_Clear_Clipboard','クリップボードの消去');
+define('lang_Clear_Clipboard_Confirm','クリップボード内のデータを消去しますか?');
+define('lang_Files_ON_Clipboard','クリップボードにファイルがあります。');
+define('lang_Copy_Cut_Size_Limit','選択したファイルが/フォルダを％sするには大きすぎます。 リミット: %d MB/操作'); // %s = cut or copy
+define('lang_Copy_Cut_Count_Limit','選択したファイルが/フォルダを％sするには大きすぎます。 リミット: %d ファイル/操作'); // %s = cut or copy
+define('lang_Copy_Cut_Not_Allowed','ファイルを %s する許可がありません。'); // %s(1) = cut or copy, %s(2) = files or folders
+define('lang_Aviary_No_Save', 'イメージを保存できませんでした');
+define('lang_Zip_No_Extract', '解凍できませんでした。ファイルが破損している可能性があります。');
+define('lang_Zip_Invalid', '拡張子がサポートされてません。有効：　zip, gz, tar.');
+define('lang_Dir_No_Write', '選択したディレクトリに書き込み権限がありません');
+define('lang_Function_Disabled', '%s はサーバによって無効にされています。'); // %s = cut or copy
+define('lang_File_Permission', 'ファイルの権限');
+define('lang_File_Permission_Not_Allowed', '%s の権限変更は許可されていません。'); // %s = files or folders
+define('lang_File_Permission_Recursive', '内包するファイルに適用しますか?');
+define('lang_File_Permission_Wrong_Mode', "供給された権限が正しくありません。");
+define('lang_User', 'ユーザー');
+define('lang_Group', 'グループ');
+define('lang_Yes', 'はい');
+define('lang_No', 'いいえ');
+define('lang_Lang_Not_Found', '言語がみつかりませんでした。');
+define('lang_Lang_Change', '言語の変更');
+define('lang_File_Not_Found', '言語ファイルがみつかりませんでした。');
+define('lang_File_Open_Edit_Not_Allowed', 'このファイルを%sことができませんでした。'); // %s = open or edit
+define('lang_Edit', '編集する');
+define('lang_Edit_File', "ファイルを編集");
+define('lang_File_Save_OK', "ファイルの保存が完了しました。");
+define('lang_File_Save_Error', "ファイルの保存時にエラーが発生しました。");
+define('lang_New_File','新規ファイル');
+define('lang_No_Extension','ファイルの拡張子を指定してください。');
+define('lang_Valid_Extensions','有効な拡張子: %s'); // %s = txt,log etc.
 
 ?>
