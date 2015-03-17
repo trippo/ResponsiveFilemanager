e3ecb69f7779b61b9e2521e9b7631d08f0b9acf2
rkgrep <1@grep.su>
2015-03-17 09:34:38 +0600

    Download script code style

--- filemanager/force_download.php
+++ filemanager/force_download.php
@@ -1,7 +1,10 @@
 <?php
+
 $config = include 'config/config.php';
+
 //TODO switch to array
 extract($config, EXTR_OVERWRITE);
+
 include 'include/utils.php';
 
 if ($_SESSION['RF']["verify"] != "RESPONSIVEfilemanager")
@@ -12,47 +15,50 @@ if ($_SESSION['RF']["verify"] != "RESPONSIVEfilemanager")
 
 include 'include/mime_type_lib.php';
 
-if(strpos($_POST['path'],'/')===0
-    || strpos($_POST['path'],'../')!==FALSE
-    || strpos($_POST['path'],'./')===0)
+if (
+	strpos($_POST['path'], '/') === 0
+	|| strpos($_POST['path'], '../') !== false
+	|| strpos($_POST['path'], './') === 0
+)
 {
 	response('wrong path', 400)->send();
 	exit;
 }
 
 
-if(strpos($_POST['name'],'/')!==FALSE)
+if (strpos($_POST['name'], '/') !== false)
 {
 	response('wrong path', 400)->send();
 	exit;
 }
 
-$path=$current_path.$_POST['path'];
-$name=$_POST['name'];
+$path = $current_path . $_POST['path'];
+$name = $_POST['name'];
 
-$info=pathinfo($name);
-if(!in_array(fix_strtolower($info['extension']), $ext))
+$info = pathinfo($name);
+
+if ( ! in_array(fix_strtolower($info['extension']), $ext))
 {
 	response('wrong extension', 400)->send();
 	exit;
 }
 
-if (!file_exists($path.$name))
+if ( ! file_exists($path . $name))
 {
 	response('File not found', 404)->send();
 	exit;
 }
 
-$img_size = (string)(filesize($path.$name)); // Get the image size as string
+$img_size = (string) (filesize($path . $name)); // Get the image size as string
 
-$mime_type = get_file_mime_type( $path.$name ); // Get the correct MIME type depending on the file.
+$mime_type = get_file_mime_type($path . $name); // Get the correct MIME type depending on the file.
 
-response(file_get_contents($path.$name), 200, array(
-	'Pragma' => 'private',
-	'Cache-control' => 'private, must-revalidate',
-	'Content-Type' => $mime_type,
-	'Content-Length' => $img_size,
-	'Content-Disposition' => 'attachment; filename="'.($name).'"'
+response(file_get_contents($path . $name), 200, array(
+	'Pragma'              => 'private',
+	'Cache-control'       => 'private, must-revalidate',
+	'Content-Type'        => $mime_type,
+	'Content-Length'      => $img_size,
+	'Content-Disposition' => 'attachment; filename="' . ($name) . '"'
 ))->send();
 
 exit;
