57a0c91d0e0ff95af2c0cdf00a32846c9fc3a2cf
Alberto Peripolli <info@albertoperipolli.com>
2016-05-11 18:11:15 +0200

    Version 9.10.2
    update download script

--- filemanager/force_download.php
+++ filemanager/force_download.php
@@ -2,6 +2,7 @@
 
 $config = include 'config/config.php';
 
+
 //TODO switch to array
 extract($config, EXTR_OVERWRITE);
 
@@ -54,92 +55,66 @@ $file_ext   = $info['extension'];
 $file_path  = $path . $name;
 
 // make sure the file exists
-if (is_file($file_path))
+if (is_file($file_path) && is_readable($file_path))
 {
-	@ini_set('zlib.output_compression', 'Off');
 
+    $size = filesize($file_path);
+    $file_name = rawurldecode($file_name);
+    $mime_type = get_file_mime_type($file_path);
 
-	$file_size  = filesize($file_path);
-	$file = @fopen($file_path,"rb");
-	if ($file)
-	{
-		// set the headers, prevent caching
-		header("Pragma: public");
-		header("Expires: -1");
-		header("Cache-Control: public, must-revalidate, post-check=0, pre-check=0");
-		header("Content-Disposition: attachment; filename=\"$file_name\"");
+    @ob_end_clean();
+    if(ini_get('zlib.output_compression'))
+    ini_set('zlib.output_compression', 'Off');
+    header('Content-Type: ' . $mime_type);
+    header('Content-Disposition: attachment; filename="'.$file_name.'"');
+    header("Content-Transfer-Encoding: binary");
+    header('Accept-Ranges: bytes');
 
-		// set the mime type based on extension, add yours if needed.
-		$ctype = get_file_mime_type($file_path);
-        header("Content-Type: " . $ctype);
+    if(isset($_SERVER['HTTP_RANGE']))
+    {
+        list($a, $range) = explode("=",$_SERVER['HTTP_RANGE'],2);
+        list($range) = explode(",",$range,2);
+        list($range, $range_end) = explode("-", $range);
+        $range=intval($range);
+        if(!$range_end) {
+            $range_end=$size-1;
+        } else {
+            $range_end=intval($range_end);
+        }
 
-		//check if http_range is sent by browser (or download manager)
-		if(isset($_SERVER['HTTP_RANGE']))
-		{
-			list($size_unit, $range_orig) = explode('=', $_SERVER['HTTP_RANGE'], 2);
-			if ($size_unit == 'bytes')
-			{
-				//multiple ranges could be specified at the same time, but for simplicity only serve the first range
-				//http://tools.ietf.org/id/draft-ietf-http-range-retrieval-00.txt
-				list($range, $extra_ranges) = explode(',', $range_orig, 2);
-			}
-			else
-			{
-				$range = '';
-				header('HTTP/1.1 416 Requested Range Not Satisfiable');
-				exit;
-			}
-		}
-		else
-		{
-			$range = '';
-		}
+        $new_length = $range_end-$range+1;
+        header("HTTP/1.1 206 Partial Content");
+        header("Content-Length: $new_length");
+        header("Content-Range: bytes $range-$range_end/$size");
+    } else {
+        $new_length=$size;
+        header("Content-Length: ".$size);
+    }
 
-		//figure out download piece from range (if set)
-		list($seek_start, $seek_end) = explode('-', $range, 2);
+    $chunksize = 1*(1024*1024);
+    $bytes_send = 0;
+    if ($file = fopen($file_path, 'r'))
+    {
+        if(isset($_SERVER['HTTP_RANGE']))
+        fseek($file, $range);
 
-		//set start and end based on range (if set), else set defaults
-		//also check for invalid ranges.
-		$seek_end   = (empty($seek_end)) ? ($file_size - 1) : min(abs(intval($seek_end)),($file_size - 1));
-		$seek_start = (empty($seek_start) || $seek_end < abs(intval($seek_start))) ? 0 : max(abs(intval($seek_start)),0);
+        while(!feof($file) &&
+            (!connection_aborted()) &&
+            ($bytes_send<$new_length)
+        )
+        {
+            $buffer = fread($file, $chunksize);
+            echo($buffer);
+            flush();
+            $bytes_send += strlen($buffer);
+        }
+        fclose($file);
+    } else {
+        die('Error - can not open file.');
+    }
 
-		//Only send partial content header if downloading a piece of the file (IE workaround)
-		if ($seek_start > 0 || $seek_end < ($file_size - 1))
-		{
-			header('HTTP/1.1 206 Partial Content');
-			header('Content-Range: bytes '.$seek_start.'-'.$seek_end.'/'.$file_size);
-			header('Content-Length: '.($seek_end - $seek_start + 1));
-		}
-		else
-		  header("Content-Length: $file_size");
+    die();
 
-		header('Accept-Ranges: bytes');
-
-		set_time_limit(0);
-		fseek($file, $seek_start);
-
-		while(!feof($file)) 
-		{
-			print(@fread($file, 1024*8));
-			ob_flush();
-			flush();
-			if (connection_status()!=0) 
-			{
-				@fclose($file);
-				exit;
-			}
-		}
-
-		// file save was a success
-		@fclose($file);
-		exit;
-	}
-	else 
-	{
-		// file couldn't be opened
-		header("HTTP/1.0 500 Internal Server Error");
-		exit;
-	}
 }
 else
 {
