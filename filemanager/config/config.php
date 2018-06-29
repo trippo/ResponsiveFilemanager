83bd85055ec23bb37a1bbdf9d74798fbb4c8716e
mayrk <40301570+mayrk@users.noreply.github.com>
2018-06-29 22:57:19 +0200

    Update config.php

--- filemanager/config/config.php
+++ filemanager/config/config.php
@@ -336,6 +336,7 @@ $config = array(
 	'preview_text_files'                      => true, // eg.: txt, log etc.
 	'edit_text_files'                         => true, // eg.: txt, log etc.
 	'create_text_files'                       => true, // only create files with exts. defined in $editable_text_file_exts
+	'download_files'			  => true, // allow download files or just preview
 
 	// you can preview these type of files if $preview_text_files is true
 	'previewable_text_file_exts'              => array( "bsh", "c","css", "cc", "cpp", "cs", "csh", "cyc", "cv", "htm", "html", "java", "js", "m", "mxml", "perl", "pl", "pm", "py", "rb", "sh", "xhtml", "xml","xsl" ),
