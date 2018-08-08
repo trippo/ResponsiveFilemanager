8ce250a487ef28c1fbd50159786dd062a2c6dc03
Mikołaj Pich <m.pich@outlook.com>
2018-08-08 20:59:41 +0200

    Optimize gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -19,77 +19,55 @@ elixir(function (mix) {
 
     mix.styles(
         [
-            "../../../node_modules/bootstrap/docs/assets/css/bootstrap.css",
-            "../../../node_modules/bootstrap/docs/assets/css/bootstrap-responsive.css",
-            "lib.css",
-            "../../../node_modules/bootstrap-modal/css/bootstrap-modal.css",
-            "../../../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css",
-            "style.css"
+            '../../../node_modules/bootstrap/docs/assets/css/bootstrap.css',
+            '../../../node_modules/bootstrap/docs/assets/css/bootstrap-responsive.css',
+            '../../../node_modules/bootstrap-modal/css/bootstrap-modal.css',
+            '../../../node_modules/jquery-contextmenu/dist/jquery.contextMenu.css',
+            'lib.css',
+            'style.css'
         ],
         'filemanager/css/style.css',
         'resources/tmp/css'
     );
 
     mix.styles(
-      ["rtl-style.less"],
+      ['rtl-style.less'],
       'filemanager/css/rtl-style.css',
       'resources/assets/less'
     );
 
     mix.scripts(
         [
-            "bootstrap/js/bootstrap-transition.js",
-            "bootstrap/js/bootstrap-affix.js",
-            "bootstrap/js/bootstrap-dropdown.js",
-            "bootstrap/js/bootstrap-alert.js",
-            "bootstrap/js/bootstrap-button.js",
-            "bootstrap/js/bootstrap-collapse.js",
-            "bootstrap/js/bootstrap-dropdown.js",
-            "bootstrap/js/bootstrap-modal.js",
-            "bootstrap/js/bootstrap-tooltip.js",
-            "bootstrap/js/bootstrap-popover.js",
-            "bootstrap/js/bootstrap-scrollspy.js",
-            "bootstrap/js/bootstrap-tab.js",
-            "bootstrap/js/bootstrap-typeahead.js",
-            "bootstrap-lightbox/js/bootstrap-lightbox.js",
-            "jquery-contextmenu/dist/jquery.contextMenu.js",
-            "vanilla-lazyload/dist/lazyload.js",
-            "jquery-scrollstop/jquery.scrollstop.js",
-            "bootbox.js/bootbox.js",
-            "jquery-touchswipe/jquery.touchSwipe.js",
-            "bootstrap-modal/js/bootstrap-modalmanager.js",
-            "bootstrap-modal/js/bootstrap-modal.js",
-            "clipboard/dist/clipboard.js",
-            "jquery-ui-touch-punch/jquery.ui.touch-punch.js",
+            'bootstrap/js/bootstrap-transition.js',
+            'bootstrap/js/bootstrap-affix.js',
+            'bootstrap/js/bootstrap-dropdown.js',
+            'bootstrap/js/bootstrap-alert.js',
+            'bootstrap/js/bootstrap-button.js',
+            'bootstrap/js/bootstrap-collapse.js',
+            'bootstrap/js/bootstrap-dropdown.js',
+            'bootstrap/js/bootstrap-modal.js',
+            'bootstrap/js/bootstrap-tooltip.js',
+            'bootstrap/js/bootstrap-popover.js',
+            'bootstrap/js/bootstrap-scrollspy.js',
+            'bootstrap/js/bootstrap-tab.js',
+            'bootstrap/js/bootstrap-typeahead.js',
+            'bootstrap-lightbox/js/bootstrap-lightbox.js',
+            'jquery-contextmenu/dist/jquery.contextMenu.js',
+            'vanilla-lazyload/dist/lazyload.js',
+            'jquery-scrollstop/jquery.scrollstop.js',
+            'bootbox.js/bootbox.js',
+            'jquery-touchswipe/jquery.touchSwipe.js',
+            'bootstrap-modal/js/bootstrap-modalmanager.js',
+            'bootstrap-modal/js/bootstrap-modal.js',
+            'clipboard/dist/clipboard.js',
+            'jquery-ui-touch-punch/jquery.ui.touch-punch.js',
         ],
         'filemanager/js/plugins.js',
         'node_modules'
     );
 
-    mix.copy(
-        [
-            "node_modules/blueimp-file-upload/js/jquery.fileupload-audio.js",
-            "node_modules/blueimp-file-upload/js/jquery.fileupload-image.js",
-            "node_modules/blueimp-file-upload/js/jquery.fileupload-process.js",
-            "node_modules/blueimp-file-upload/js/jquery.fileupload-ui.js",
-            "node_modules/blueimp-file-upload/js/jquery.fileupload-validate.js",
-            "node_modules/blueimp-file-upload/js/jquery.fileupload-video.js",
-            "node_modules/blueimp-file-upload/js/jquery.fileupload.js",
-            "node_modules/blueimp-file-upload/js/jquery.iframe-transport.js",
-            "node_modules/bootstrap-lightbox/js/bootstrap-lightbox.js",
-            "node_modules/bootstrap-modal/js/bootstrap-modal.js",
-            "node_modules/bootstrap-modal/js/bootstrap-modalmanager.js",
-        ],
-        'filemanager/js/'
-    );
-
-    mix.copy(
-        [
-            "node_modules/blueimp-file-upload/css/jquery.fileupload.css",
-            "node_modules/blueimp-file-upload/css/jquery.fileupload-ui.css",
-        ],
-        'filemanager/css/'
-    );
+    mix.copy('node_modules/blueimp-file-upload/js', 'filemanager/js/');
+    mix.copy('node_modules/blueimp-file-upload/css', 'filemanager/css/');
 
     mix.scripts(
         ['include.js'],
