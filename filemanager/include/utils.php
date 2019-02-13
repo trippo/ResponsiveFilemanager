fe8062b1a20a192b0250bbe8c776f75c1ad0bce1
Tim McLaughlin <tim.j.mclaughlin@gmail.com>
2019-02-13 12:30:40 -0800

    Include the number of color channels when estimating the memory usage of an image.

--- filemanager/include/utils.php
+++ filemanager/include/utils.php
@@ -847,13 +847,14 @@ function image_check_memory_usage($img, $max_breedte, $max_hoogte)
             $image_properties = getimagesize($img);
             $image_width = $image_properties[0];
             $image_height = $image_properties[1];
+            $image_bits = 0;
+            $image_channels = 0;
             if (isset($image_properties['bits'])) {
                 $image_bits = $image_properties['bits'];
-            } else {
-                $image_bits = 0;
+                $image_channels = isset($image_properties['channels']) ? $image_properties['channels'] : 1;
             }
-            $image_memory_usage = $K64 + ($image_width * $image_height * ($image_bits >> 3) * 2);
-            $thumb_memory_usage = $K64 + ($max_breedte * $max_hoogte * ($image_bits >> 3) * 2);
+            $image_memory_usage = $K64 + ($image_width * $image_height * ($image_bits * $image_channels / 8) * 2);
+            $thumb_memory_usage = $K64 + ($max_breedte * $max_hoogte * ($image_bits * $image_channels / 8) * 2);
             $memory_needed = abs(intval($memory_usage + $image_memory_usage + $thumb_memory_usage));
 
             if ($memory_needed > $memory_limit) {
