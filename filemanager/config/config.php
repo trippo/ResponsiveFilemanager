8f381ced388212d69e90e0edb0533dfce6ab7454
Gijsdev <clossd@protonmail.com>
2021-02-25 13:17:05 +0100

    Remove wrong use of mb_http_input

--- filemanager/config/config.php
+++ filemanager/config/config.php
@@ -6,7 +6,6 @@ if (session_id() == '') {
 
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
-mb_http_input('UTF-8');
 mb_language('uni');
 mb_regex_encoding('UTF-8');
 ob_start('mb_output_handler');
